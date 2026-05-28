458ec7e43e3daaeb8b63fced9bb294eda85adc8e
SumitDesai-21 <sumit.desai3433@gmail.com>
2026-05-28 16:25:09 +0530

    Added setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen bg-gray-100'>
+      <div className=' m-auto bg-black '>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default App
